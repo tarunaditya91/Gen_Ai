51a304bc155605fbc0c7fa7bdc7257b57c434818
tarunaditya91 <shankertarunaditya369@gmail.com>
2025-04-15 02:00:23 +0530

    day2 over

--- chatgpt1.py
+++ chatgpt1.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import os
 load_dotenv()
-
+import json
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -11,6 +11,7 @@ You are an AI assistant who is specialized in math.
 Don't answer questions that are not related to math. 
 For unrelated queries, explain that you're limited to math help only.
 Give examples wherever possible — more examples give better output.
+you should think multiple time befor giveing the answer.
 
 Rules:
 1.Follow the strict JSON  output as per output schema.
@@ -23,20 +24,32 @@ Output Format:
 example :
 Input : what is 2+2.
 output : {{steps:"analyse",content:"the user is intrested in maths and asking a basic arthermatic operation"}}
-output : {{step: "think","content:"To performe the addition i must go left to right and follow airthematic operations.}}
+output : {{step:"think","content:"To performe the addition i must go left to right and follow airthematic operations.}}
 output : {{step:"output",content:"4"}}
 output : {{step:"validate",content:"seems like 4 is correct ans for 2+2"}}
-output : {{step: "}}
 """
 
-result = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    temperature=0.5,
-    max_tokens=200,
-    messages=[  # <-- fixed from 'message' to 'messages'
-        {'role': "system","content": system_prompt},
-        {"role": "user", "content": "what is 2+2"}
-    ]
-)
+messages = [
+    {'role': "system","content": system_prompt}
+    
+]
 
-print(result.choices[0].message.content)  # <-- fixed from 'choice' to 'choices'
+query = input(":->")
+messages.append({"role": "user","content": query})
+
+
+while True:
+    result = client.chat.completions.create(
+    model="gpt-4o",
+    response_format={"type": "json_object"},
+    messages=messages
+    )
+    parsed_response = json.loads(result.choices[0].message.content)
+    messages.append({'role':"assistant","content": json.dumps(parsed_response)})
+    if parsed_response.get("step")!="output":
+        print(f"brain:->{parsed_response.get("content")}")
+        continue
+    print(f"boat:->{parsed_response.get("content")}")
+    break
+
+
